9e4cc82f22928a4a6fec3e8c6cde1b322a969d2b
Jon Atack <jon@atack.com>
2019-09-02 22:01:21 +0200

    Consolidate, improvements, docstrings

--- src/main.lisp
+++ src/main.lisp
@@ -13,8 +13,6 @@
                 #:make-uri)
   (:import-from #:jsown
                 #:parse)
-  (:import-from #:cl-strings
-                #:clean)
   (:import-from #:bitcoin-core-pr-reviews/src/pull-request-query
                 #:pull-request-query)
   (:export #:graphql_post))
@@ -28,37 +26,67 @@
 (defparameter *github-user* "jonatack")
 (defparameter *github-token* "token")
 (defparameter *postgres-password* "")
+(defparameter *github-queries-path*
+  "/home/jon/common-lisp/bitcoin-core-pr-reviews/src/github-graphql-queries/")
 
 (defun db-connect ()
+  "Connect to the PostgreSQL database."
   (mito:connect-toplevel
    :postgres
    :database-name "bitcoin_core_pr_reviews"
    :username "jon"
    :password *postgres-password*))
 
+(defun get-pull-request () (github-query "pull_request"))
+(defun get-pull-requests () (github-query "pull_requests"))
+
+(defun github-query (filename)
+  "Perform a GitHub API query as specified in the given GraphQL file."
+  (check-type filename simple-string)
+  (graphql-post
+   (read-file-into-string
+    (concatenate 'string *github-queries-path* filename ".graphql"))))
+
 (defun graphql-post (query &key raw verbose)
-  (check-type query simple-string)
+  "GraphQL POST request for the GitHub API queries."
+  (declare (type simple-string query))
   (let* ((uri (make-uri :scheme "https" :host *github-host* :path *github-path*))
-         (data (stripped query))
+         (data (strip query))
          (response (post uri :headers (headers) :content data :verbose verbose)))
     (if raw response (parse response))))
 
 (defun headers ()
+  "HTTP POST headers for the GitHub API GraphQL queries."
   `(("accept" . "application/vnd.github.starfox-preview+json")
     ("user-agent" . ,*github-user*)
     ("authorization" . ,*github-token*)))
 
-(defun stripped (string)
+(defun strip (string)
+  "Remove newline characters, leading/trailing spaces, and multiple spaces."
+  (declare (type simple-string string))
   (clean (remove #\Newline string)))
 
-(defun read-file (path)
-  (with-open-file (stream path :direction :input :if-does-not-exist nil)
+(defun clean (string &key (char #\space))
+  "Returns a string with leading/trailing spaces removed and multiple spaces
+  replaced by one."
+  (declare (type simple-string string)
+           (type character char))
+  (let ((trimmed (string-trim (string char) string))
+        (char-found nil))
+    (with-output-to-string (stream)
+      (loop for c across trimmed do
+               (if (char= c char)
+                   (when (not char-found)
+                     (write-char c stream)
+                     (setq char-found t))
+                   (progn
+                     (if char-found (setf char-found nil))
+                     (write-char c stream)))))))
+
+(defun read-file-into-string (pathname)
+  "Return the contents of the file denoted by PATHNAME as a fresh string."
+  (declare (type simple-string pathname))
+  (with-open-file (stream pathname :direction :input :if-does-not-exist nil)
     (let ((string (make-string (file-length stream))))
       (read-sequence string stream)
       string)))
-
-(defun get-pull-request ()
-  (graphql-post (read-file "./github-graphql-queries/pull_request.graphql")))
-
-(defun get-pull-requests ()
-  (graphql-post (read-file "./github-graphql-queries/pull_requests.graphql")))
